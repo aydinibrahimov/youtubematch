b62360ee644cd822a575140e54d6cd76f8d6bb4f
Admin <aydin.ibrahimov.a@asoiu.edu.az>
2022-11-21 09:53:51 +0400

    is commiting

--- src/main/java/com/company/domain/Course.java
+++ src/main/java/com/company/domain/Course.java
@@ -1,4 +1,10 @@
 package com.company.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
+    private Long id;
 }
--- src/main/java/com/company/domain/User.java
+++ src/main/java/com/company/domain/User.java
@@ -1,4 +1,11 @@
 package com.company.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    private Long id;
 }
